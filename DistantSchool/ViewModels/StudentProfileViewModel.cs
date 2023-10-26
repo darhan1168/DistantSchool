4e017302389dc8b4a377e61cc60048efe1230cb9
darhan1168 <darinahanina04@gmail.com>
2023-10-26 15:34:31 +0200

    Fix problem with updating Stident

--- DistantSchool/ViewModels/StudentProfileViewModel.cs
+++ DistantSchool/ViewModels/StudentProfileViewModel.cs
@@ -8,4 +8,5 @@ public class StudentProfileViewModel : BaseProfileViewModel
     public string Address { get; set; }
     public int GradeLevel { get; set; }
     public Class Class { get; set; }
+    public int ClassID { get; set; }
 }
